babc7b53de0b4d39e479b4b60554201d97abf820
Nick Mileson <nmileson@gmail.com>
2013-03-13 00:05:01 -0700

    all color anim

--- RPi/ledstrip/ledstrip.py
+++ RPi/ledstrip/ledstrip.py
@@ -7,20 +7,45 @@ leds.allOff()
 def pulse():
    for dir in range(2):
       for r in range(128):
-         color = 0x00 | abs(128 * dir - r)
+         color = abs(128 * dir - r)
          leds.allColor(color, color, color)
          time.sleep(.001)
 
-def colortest(): 
-   for r in range(256):
-      red = 0x00 | r
-      leds.allColor(0xFF, red, 0x00)
+def colortest(isr, isg, isb): 
+   for color in range(256):
+      red = isr * color
+      green = isg * color
+      blue = isb * color
+      leds.allColor(red, green, blue)
       time.sleep(.001)
 
+def togglecolor(r, g, b, isback):
+   red = r * 255
+   green = g * 255
+   blue = b * 255
+   for c in range(256):
+      if isback == True:
+         c = 255 - c
+      if r == -1:
+         red = c
+      if g == -1:
+         green = c
+      if b == -1:
+         blue = c
+
+      leds.allColor(red, green, blue)
+      time.sleep(.005)
+
+def allcolors():
+   leds.allOff()
+   togglecolor(-1, 0, 0, False)
+   togglecolor(1, -1, 0, False)
+   togglecolor(-1, 1, 0, True)
+   togglecolor(0, 1, -1, False)
+   togglecolor(0, -1, 1, True)
+   togglecolor(-1, 0, 1, False)
+   togglecolor(1, -1, 1, False)
+   togglecolor(-1, -1, -1, True)
+   
 while True:
-   pulse()
-   leds.goHawks()
-   pulse()
-   pulse()
-   colortest()
-   pulse()
+   allcolors()
